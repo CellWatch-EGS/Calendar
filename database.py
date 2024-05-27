a21112e9168e0c8214360503e985b0b0610b219a
GulhermeC <gjscraveiro@ua.pt>
2024-05-27 20:09:00 +0100

    test

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from databases import Database
+import os
 
-DATABASE_URL = "postgresql://calendar:password123@calendar_db/calendar"
+#DATABASE_URL = "postgresql://calendar:password123@calendar_db/calendar"
+DATABASE_URL = os.environ.get('DATABASE_URI')
 database = Database(DATABASE_URL)
